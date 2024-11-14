1450952c121a649d758b5a8f61cc1bc83be84ac7
Zoy Nabben <zoy@nabben.info>
2024-11-14 08:51:32 +0100

    2024-11-14 (08:51)

---
+++ sql-opdracht-1.sql
@@ -0,0 +1,3 @@
+-- Create a report that shows the CategoryName and Description 
+-- from the categories table sorted by CategoryName. 
+SELECT CategoryName, Description FROM `categories` GROUP BY CategoryName
